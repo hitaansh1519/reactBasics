7e7584f4ba5c5dd112ee60b0a8b7c86b95335163
Hitaansh Maheshwary <hitaansh2003@gmail.com>
2025-07-03 13:41:13 +0530

    Add Card component and integrate with App; include image asset

--- TailwindProps/src/App.jsx
+++ TailwindProps/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4' >Tailwind CSS</h1>
+      <Card username = "Hitaansh" button = "Click ker"/>
+      <Card username = "Maheshwary" button = "Isko Click mat kerna"/>
     </>
   )
 }
